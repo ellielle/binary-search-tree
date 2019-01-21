9c97bb56e2a4ee2069f208a0d7811a20d8578daf
ellielle <40385743+ellielle@users.noreply.github.com>
2019-01-21 06:44:00 -0500

    change add_node to non-recursive

--- lib/tree.rb
+++ lib/tree.rb
@@ -18,10 +18,7 @@ class BinarySearchTree
 
   def build_tree(arr)
     return puts "\nInvalid data." if arr.nil? || !arr.is_a?(Array)
-    arr.each do |element|
-      add_node(element)
-      @size += 1
-    end
+    arr.each { |element| add_node(element) }
   end
 
   def add_node(value)
@@ -29,21 +26,24 @@ class BinarySearchTree
       @root = Node.new(value)
     else
       temp_root = @root
-
       until temp_root.nil?
         parent = temp_root
-        if value < temp_root.value
-          temp_root = temp_root.left
-        else
-          temp_root = temp_root.right
-        end
+        value < temp_root.value ? temp_root = temp_root.left : temp_root = temp_root.right
       end
-
-      if value < parent.value
-        parent.left = Node.new(value)
-      else
-        parent.right = Node.new(value)
-      end
+      value < parent.value ? parent.left = Node.new(value) : parent.right = Node.new(value)
     end
+    @size += 1
   end
+
+  def breadth_first_search(value)
+    #TODO use array to make queue of child nodes not yet visited. QUEUE -> FIFO
+    #     F       start at F -> [D, J] -> [B,E,G,K]
+    #   D ^ J
+    # B^E  G^K
+  end
+  def depth_first_search(value)
+    #TODO use array as a stack. STACK -> LIFO
+    # start at F -> [D, J] -> [B,E,D,J]
+  end
+  def dfs_rec(value); end #recursive depth first
 end
