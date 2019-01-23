d423ae44d7df5586a5a4972d6c05831417e13c18
ellielle <40385743+ellielle@users.noreply.github.com>
2019-01-23 02:51:31 -0500

    fix recursive search

--- lib/test.rb
+++ lib/test.rb
@@ -1,9 +1,30 @@
 require_relative 'tree'
 
-unsorted_arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-sorted_arr = [1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345]
-
-sorted_bst = BinarySearchTree.new(sorted_arr)
-unsorted_bst = BinarySearchTree.new(unsorted_arr)
-
-p unsorted_bst.inspect
+random_arr = Array.new(20) { rand(1...100) }
+random_arr.push(27) #pushing a non random number to make sure the method works properly
+control_arr = [1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 645]
+control_bst = BinarySearchTree.new(control_arr)
+random_bst = BinarySearchTree.new(random_arr)
+#breadth-first searching
+control = control_bst.breadth_first_search(9)
+search_random = random_bst.breadth_first_search(48)
+search_not_so_random = random_bst.breadth_first_search(27)
+print control.nil? ? "Value not found. " : "#{control} found in tree. "
+print search_random.nil? ? "Value not found. " : "#{search_random} found in tree. "
+print search_not_so_random.nil? ? "Value not found. " : "#{search_not_so_random} found in tree. "
+puts
+#depth-first searching
+control = control_bst.depth_first_search(67)
+search_random = random_bst.depth_first_search(48)
+search_not_so_random = random_bst.depth_first_search(27)
+print control.nil? ? "Value not found. " : "#{control} found in tree. "
+print search_random.nil? ? "Value not found. " : "#{search_random} found in tree. "
+print search_not_so_random.nil? ? "Value not found. " : "#{search_not_so_random} found in tree. "
+puts
+#depth-first recursive
+control = control_bst.dfs_rec(7)
+search_random = random_bst.dfs_rec(48)
+search_not_so_random = random_bst.dfs_rec(27)
+print control.nil? ? "Value not found. " : "#{control} found in tree. "
+print search_random.nil? ? "Value not found. " : "#{search_random} found in tree. "
+print search_not_so_random.nil? ? "Value not found. " : "#{search_not_so_random} found in tree. "
--- lib/tree.rb
+++ lib/tree.rb
@@ -36,14 +36,38 @@ class BinarySearchTree
   end
 
   def breadth_first_search(value)
-    #TODO use array to make queue of child nodes not yet visited. QUEUE -> FIFO
-    #     F       start at F -> [D, J] -> [B,E,G,K]
-    #   D ^ J
-    # B^E  G^K
+    return if @root.nil? || @root.value == value
+    queue = [@root]
+    current_level = @root
+    until queue.empty?
+      return value if current_level.value == value
+      queue.shift
+      queue.push(current_level.left) unless current_level.left.nil?
+      queue.push(current_level.right) unless current_level.right.nil?
+      current_level = queue[0]
+    end
+    nil
   end
+
   def depth_first_search(value)
-    #TODO use array as a stack. STACK -> LIFO
-    # start at F -> [D, J] -> [B,E,D,J]
+    return if @root.nil? || @root.value == value
+    stack = [@root]
+    current_level = @root
+    until stack.empty?
+      return value if current_level.value == value
+      stack.shift
+      stack.unshift(current_level.right) unless current_level.right.nil?
+      stack.unshift(current_level.left) unless current_level.left.nil?
+      current_level = stack[0]
+    end
+    nil
   end
-  def dfs_rec(value); end #recursive depth first
+
+  def dfs_rec(value, current_node = @root)
+    return nil if current_node.nil?
+    return current_node.value if current_node.value == value
+    result = dfs_rec(value, current_node.left) unless current_node.left.nil?
+    return result unless result.nil?
+    dfs_rec(value, current_node.right) unless current_node.right.nil?
+  end
 end
